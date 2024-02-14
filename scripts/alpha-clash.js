9b6d1f5753d12ff5c3e55739649635421731a764
rizad123 <anomorrz1@gmail.com>
2024-02-14 23:25:17 +0600

    first

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,19 @@
+                     // Methor -- 1
+
+function play(){
+    
+    const homeSection = document.getElementById( "home-screen");
+    homeSection.classList.add('hidden');
+
+    const playgroundSection = document.getElementById('play-ground')
+    playgroundSection.classList.remove('hidden');
+}
+
+
+
+
+
+
+
+
+
---
+++ scripts/utility.js
@@ -0,0 +1,31 @@
+
+// function hideElementById(elementId){
+//     const element = document.getElementById(elementId);
+//     element.classList.add('hidden');
+// }
+
+// function showElementById(elementId){
+//     const element = document.getElementById(elementId);
+//     element.classList.remove('hidden');
+// }
+
+
+
+
+// // Genarate Rendon Alphabet function
+
+// function getARandomAlphabet(){
+//     const alphabetString  = 'abcdefghijklmnopqrstuvwxyz';
+//     const alphabets = alphabetString.split('');
+
+
+//     const randomNumber = Math.random() * 25;
+//     const index = Math.round(randomNumber);
+ 
+
+
+//     const alphabet = alphabets[index];
+//     console.log(index, alphabet);
+//     return alphabet
+
+// }
