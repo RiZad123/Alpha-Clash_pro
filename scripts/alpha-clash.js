b0ccd231683eca656965d008d3cd72be1c2cd8ae
rizad123 <anomorrz1@gmail.com>
2024-02-15 00:16:08 +0600

    design Done

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,16 +1,32 @@
                      // Methor -- 1
 
-function play(){
+// function play(){
     
-    const homeSection = document.getElementById( "home-screen");
-    homeSection.classList.add('hidden');
+//     const homeSection = document.getElementById( "home-screen");
+//     homeSection.classList.add('hidden');
 
-    const playgroundSection = document.getElementById('play-ground')
-    playgroundSection.classList.remove('hidden');
+//     const playgroundSection = document.getElementById('play-ground')
+//     playgroundSection.classList.remove('hidden');
+// }
+
+
+function continueGame(){
+      const alphabet = getARandomAlphabet();
+      console.log('Your rendom Alphabet', alphabet);
+
+      const currentAlphabetElement = document.getElementById('currentAlphabet');
+      currentAlphabetElement.innerText = alphabet;
 }
 
 
 
+// Methor -- 2
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
+}
 
 
 
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,31 +1,31 @@
+// Methor -- 2
 
-// function hideElementById(elementId){
-//     const element = document.getElementById(elementId);
-//     element.classList.add('hidden');
-// }
+function hideElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
 
-// function showElementById(elementId){
-//     const element = document.getElementById(elementId);
-//     element.classList.remove('hidden');
-// }
-
-
-
-
-// // Genarate Rendon Alphabet function
-
-// function getARandomAlphabet(){
-//     const alphabetString  = 'abcdefghijklmnopqrstuvwxyz';
-//     const alphabets = alphabetString.split('');
+function showElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
 
 
-//     const randomNumber = Math.random() * 25;
-//     const index = Math.round(randomNumber);
- 
 
 
-//     const alphabet = alphabets[index];
-//     console.log(index, alphabet);
-//     return alphabet
+// Genarate Rendon Alphabet function
 
-// }
+function getARandomAlphabet(){
+    const alphabetString  = 'abcdefghijklmnopqrstuvwxyz/';
+    const alphabets = alphabetString.split('');
+
+
+    const randomNumber = Math.random() * 26;
+    const index = Math.round(randomNumber);
+
+
+    const alphabet = alphabets[index];
+    console.log(index, alphabet);
+    return alphabet
+
+}
